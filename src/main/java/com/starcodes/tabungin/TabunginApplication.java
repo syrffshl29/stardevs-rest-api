2900c27ed7a783b03678dd01ebfba3ac11fd30bd
ricky-blip <ricky.rinaldy77@gmail.com>
2025-07-13 21:48:53 +0700

    first commit

---
+++ src/main/java/com/starcodes/tabungin/TabunginApplication.java
@@ -0,0 +1,13 @@
+package com.starcodes.tabungin;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TabunginApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TabunginApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/starcodes/tabungin/TabunginApplicationTests.java
@@ -0,0 +1,13 @@
+package com.starcodes.tabungin;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TabunginApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
