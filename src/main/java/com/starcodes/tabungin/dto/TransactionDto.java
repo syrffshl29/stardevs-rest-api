c1f1dd4c91ea6de20bd93b1de71430b72e4d24c5
TaufikNurSidiik <taufiknursidiik@gmail.com>
2025-07-27 17:27:50 +0700

    Setter Getter

--- src/main/java/com/starcodes/tabungin/dto/TransactionDto.java
+++ src/main/java/com/starcodes/tabungin/dto/TransactionDto.java
@@ -1,5 +1,107 @@
 package com.starcodes.tabungin.dto;
 
+import jakarta.persistence.Column;
+
+import java.time.LocalDateTime;
+
 public class TransactionDto {
-	//nampilin transaksi
+    private Long id;
+    private Long targetId;
+    private Long userId;
+    private String jenisTransaksi;
+    private double jumlahTransaksi;
+    private String metodePembayaran;
+    private String statusTransaksi;
+    private String keterangan;
+    private String referensiPembayaran;
+    private LocalDateTime createdAt;
+    private LocalDateTime updatedAt;
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public Long getTargetId() {
+        return targetId;
+    }
+
+    public void setTargetId(Long targetId) {
+        this.targetId = targetId;
+    }
+
+    public Long getUserId() {
+        return userId;
+    }
+
+    public void setUserId(Long userId) {
+        this.userId = userId;
+    }
+
+    public String getJenisTransaksi() {
+        return jenisTransaksi;
+    }
+
+    public void setJenisTransaksi(String jenisTransaksi) {
+        this.jenisTransaksi = jenisTransaksi;
+    }
+
+    public double getJumlahTransaksi() {
+        return jumlahTransaksi;
+    }
+
+    public void setJumlahTransaksi(double jumlahTransaksi) {
+        this.jumlahTransaksi = jumlahTransaksi;
+    }
+
+    public String getMetodePembayaran() {
+        return metodePembayaran;
+    }
+
+    public void setMetodePembayaran(String metodePembayaran) {
+        this.metodePembayaran = metodePembayaran;
+    }
+
+    public String getStatusTransaksi() {
+        return statusTransaksi;
+    }
+
+    public void setStatusTransaksi(String statusTransaksi) {
+        this.statusTransaksi = statusTransaksi;
+    }
+
+    public String getKeterangan() {
+        return keterangan;
+    }
+
+    public void setKeterangan(String keterangan) {
+        this.keterangan = keterangan;
+    }
+
+    public String getReferensiPembayaran() {
+        return referensiPembayaran;
+    }
+
+    public void setReferensiPembayaran(String referensiPembayaran) {
+        this.referensiPembayaran = referensiPembayaran;
+    }
+
+    public LocalDateTime getCreatedAt() {
+        return createdAt;
+    }
+
+    public void setCreatedAt(LocalDateTime createdAt) {
+        this.createdAt = createdAt;
+    }
+
+    public LocalDateTime getUpdatedAt() {
+        return updatedAt;
+    }
+
+    public void setUpdatedAt(LocalDateTime updatedAt) {
+        this.updatedAt = updatedAt;
+    }
 }
