83170c838d0e4e9aae974cbbb49959a297f69734
Syarif <syarifaf29@gmail.com>
2025-08-04 18:41:19 +0700

    initial upload

---
+++ src/main/java/com/starcodes/tabungin/core/interfaces/IService.java
@@ -0,0 +1,15 @@
+package com.starcodes.tabungin.core.interfaces;
+
+import jakarta.servlet.http.HttpServletRequest;
+import org.springframework.data.domain.Pageable;
+import org.springframework.http.ResponseEntity;
+
+public interface IService <D>{
+    public ResponseEntity<Object>save(D d, HttpServletRequest request);//001-010
+    public ResponseEntity<Object>delete(Long id, D d, HttpServletRequest request);//011-020
+    public ResponseEntity<Object>update(Long id, D d, HttpServletRequest request);//021-030
+    public ResponseEntity<Object>findById(Long id, D d, HttpServletRequest request);//031-040
+    public ResponseEntity<Object>findAll(Pageable pageable, HttpServletRequest request);//041-050
+    public ResponseEntity<Object>findByParam(Pageable pageable,String column, String value, HttpServletRequest request);//051-060
+}
+
