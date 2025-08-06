981677b70ff8157b17a7636758d2b6f4d4513e31
TaufikNurSidiik <taufiknursidiik@gmail.com>
2025-08-06 14:18:01 +0700

    Detail Target Tabungan (Resp DTO, Val DTO, Service, Controller)

---
+++ src/main/java/com/starcodes/tabungin/controller/DetailTatgetController.java
@@ -0,0 +1,31 @@
+package com.starcodes.tabungin.controller;
+
+import com.starcodes.tabungin.dto.validation.ValTargetTabunganDto;
+import com.starcodes.tabungin.model.DetailTarget;
+import com.starcodes.tabungin.model.Setoran;
+import com.starcodes.tabungin.service.DetailTargetServiceImpl;
+import com.starcodes.tabungin.service.TargetServiceImpl;
+import jakarta.servlet.http.HttpServletRequest;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Pageable;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+@RestController
+@RequestMapping("/detail")
+public class DetailTatgetController {
+
+        @Autowired
+        private DetailTargetServiceImpl detailTargetServiceImpl;
+
+        @GetMapping
+        public Object findAll(HttpServletRequest request, Pageable pageable) {
+            return detailTargetServiceImpl.findAll();
+        }
+        @GetMapping("/{id}")
+        public ResponseEntity<DetailTarget> getUserById(@PathVariable Long id) {
+            return detailTargetServiceImpl.findById(id)
+                    .map(ResponseEntity::ok)
+                    .orElse(ResponseEntity.notFound().build());
+        }
+}
--- src/main/java/com/starcodes/tabungin/repository/DetailTargetRepository.java
+++ src/main/java/com/starcodes/tabungin/repository/DetailTargetRepository.java
@@ -1,9 +1,10 @@
 package com.starcodes.tabungin.repository;
 
+import com.starcodes.tabungin.model.DetailTarget;
 import com.starcodes.tabungin.model.LogAktivitas;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 @Repository
-public interface DetailTargetRepository extends JpaRepository<LogAktivitas,Long> {
+public interface DetailTargetRepository extends JpaRepository<DetailTarget,Long> {
 }
