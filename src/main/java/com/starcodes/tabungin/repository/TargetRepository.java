b4a194cf9fdebf976c5cd165f48476b2c74a1ee8
Syarif <syarifaf29@gmail.com>
2025-08-15 09:41:59 +0700

    initial upload

--- src/main/java/com/starcodes/tabungin/repository/TargetRepository.java
+++ src/main/java/com/starcodes/tabungin/repository/TargetRepository.java
@@ -4,8 +4,11 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import com.starcodes.tabungin.model.TargetTabungan;
 import org.springframework.stereotype.Repository;
 
+import java.util.List;
+
 @Repository
 public interface TargetRepository extends JpaRepository<TargetTabungan, Long> {
 
+    List<TargetTabungan>findByUserId(Long userId);
 	//CRUD model TargetTabungan
 }
